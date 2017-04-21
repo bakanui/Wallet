5a3f62e6780e83df9c1253d8add115bd3e7c05e0
bakanui <gusbhas@gmail.com>
2017-04-22 03:36:35 +0700

    Adds Log In and Register Page

--- src/main/java/com/vct/wallet/app/AppConfig.java
+++ src/main/java/com/vct/wallet/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.vct.wallet.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.43.189/android_login_api/login.php";
+    public static String URL_LOGIN = "http://vctwallet.azurewebsites.net/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.43.189/android_login_api/register.php";
+    public static String URL_REGISTER = "http://vctwallet.azurewebsites.net/register.php";
 }
