7d6258ec4a13d1790bcc84fc6c0196c6d1a11820
bakanui <gusbhas@gmail.com>
2017-04-24 06:08:53 +0700

    Reinit everything because everything gone to shit after the merge

---
+++ app/src/main/java/com/vct/wallet/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.vct.wallet.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://vctwallet.azurewebsites.net/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://vctwallet.azurewebsites.net/register.php";
+}
