4a5eef3b6086c8bcc486ec70cc2ffbdb24d4332c
Bhaskara Ida Bagus <gusbhas@gmail.com>
2017-04-25 00:41:50 +0700

    Init commit for php processes

---
+++ vctwallet-php/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "ap-cdbr-azure-southeast-b.cloudapp.net");
+define("DB_USER", "bf2044c69dd7b1");
+define("DB_PASSWORD", "0c2f7ae6");
+define("DB_DATABASE", "vctwallet");
+?>
